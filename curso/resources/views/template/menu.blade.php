5ad92bb632b147dd97bb77bf858de6945ddcb316
Rafael Carvalho <rafael.carvalho@impix.com.br>
2015-11-02 15:44:51 -0200

    Corrigindo menu.

--- curso/resources/views/template/menu.blade.php
+++ curso/resources/views/template/menu.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">PHP</a>
+            <a class="navbar-brand" href="/">PHP</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
