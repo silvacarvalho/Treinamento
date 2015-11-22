ea68d2d86f9720bbe8702143dc141fca25cdc80c
Rafael Carvalho <rafael.carvalho@impix.com.br>
2015-11-22 02:21:17 -0200

    Adicionando mudanças no Controller Feira.

--- curso/app/Http/Controllers/FeiraController.php
+++ curso/app/Http/Controllers/FeiraController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
+use App\Feira;
 use App\Http\Controllers\Controller;
 
 class FeiraController extends Controller
@@ -16,7 +15,7 @@ class FeiraController extends Controller
      */
     public function index()
     {
-        return view('feira.index');
+        return view('feira.index')->with("dados", $this->listar());
     }
 
     /**
@@ -26,7 +25,14 @@ class FeiraController extends Controller
      */
     public function create()
     {
-        // Cria um novo registro Feira.
+        try{
+            $date = date_create($_POST["data"]);
+            $_POST["data"] = date_format($date, "Y-m-d H:i");
+            $id = \DB::connection('mysql')->table('feira')->insert($_POST);
+            exit(json_encode($id));
+        } catch (\Exception $ex) {
+            exit(json_encode("Erro ao salvar feira: " . $ex->getMessage()));
+        }
     }
 
     /**
@@ -37,8 +43,9 @@ class FeiraController extends Controller
      */
     public function show($id)
     {
-        //Mostra dados de uma feira.
-        return view('feira.ver');
+        $feira = new Feira();
+        $dados = $feira->getFeira($id);
+        return view('feira.ver')->with("dados", $dados);
     }
 
     /**
@@ -50,7 +57,17 @@ class FeiraController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //Atualiza dados de uma Feira.
+        $dados = $request->all();
+        $date = date_create($dados["data"]);
+        $dados["data"] = date_format($date, "Y-m-d H:i");
+        return \DB::connection('mysql')->table('feira')
+                                        ->where('id', $id)
+                                        ->update([      'tipo'          => $dados['tipo'],
+                                                        'tema'          => $dados['tema'],
+                                                        'local'         => $dados['local'],
+                                                        'data'          => $dados['data'],
+                                                        'status'        => $dados['status'],
+                                                        'observacao'    => $dados['observacao']]);
     }
 
     /**
@@ -62,5 +79,31 @@ class FeiraController extends Controller
     public function destroy($id)
     {
         // Remove os dados de uma Feira.
+    }
+    
+    public function action_nova()
+    {
+        return view('feira.nova');
+    }
+    
+    /**
+     * Retorna um Objeto de Feiras
+     * @param Int $status Status da feira:  Acept=0/1; default=false;
+     * Se default, retorna todas as feiras, se 0, somente inativas, se 1, ativas.
+     * @return Objeto JSON
+     */
+    public function listar($status = false)
+    {
+        if($status != false){
+            return \DB::connection('mysql')->table('feira')->where('status', $status)->get();
+        }
+        return \DB::connection('mysql')->table('feira')->get();
+    }
+    
+    
+    public function manager(Request $request, $id)
+    {
+        $dados = $request->all();
+        return \DB::connection('mysql')->table('feira')->where('id', $id)->update(['status' => $dados['status']]);
     }
 }
