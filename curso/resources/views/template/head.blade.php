e09f0a4849d1b783d3bad3a21be70fa044d0e1d3
Rafael Carvalho <rafael.carvalho@impix.com.br>
2015-11-22 02:29:40 -0200

    Chamando arquivos necessários no head.php

--- curso/resources/views/template/head.blade.php
+++ curso/resources/views/template/head.blade.php
@@ -4,9 +4,23 @@
 
 <link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap-theme.min.css') }}">
+                            <!--DataTable-->
+<link rel="stylesheet" href="{{ URL::asset('dataTable/css/jquery.dataTables.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('dataTable/css/jquery.dataTables_themeroller.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('dataTable/css/jquery-ui.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('dataTable/css/dataTables.jqueryui.min.css') }}">
+                            <!--End DataTable-->
 <link rel="stylesheet" href="{{ URL::asset('font-awesome/css/font-awesome.min.css') }}">
 <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
 
-<script src="{{ URL::asset('jquery/jquery.min.js') }}"></script>
-<script src="{{ URL::asset('bootstrap/js/bootstrap.min.js') }}"></script>
+<link rel="stylesheet" href="{{ URL::asset('jquery/datetime/css/jquery.datetimepicker.full.css') }}">
+
+<script type="text/javascript" src="{{ URL::asset('jquery/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('bootstrap/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('dataTable/js/jquery.dataTables.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('dataTable/js/dataTables.jqueryui.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ URL::asset('jquery/datetime/js/jquery.datetimepicker.full.min.js') }}"></script>
+
+
 <title>Treinamento</title>
